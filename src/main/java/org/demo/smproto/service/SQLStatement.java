1b819d3019d5c2ac7eb29abde8d7928b41dcc77b
sotudeko <sotudeko@sonatype.com>
2020-05-19 21:24:23 +0100

    0.71

--- src/main/java/org/demo/smproto/service/SQLStatement.java
+++ src/main/java/org/demo/smproto/service/SQLStatement.java
@@ -25,7 +25,7 @@ public class SQLStatement {
 	public static String OpenLicenseViolations = "select time_period_start as label, sum(OPEN_COUNT_AT_TIME_PERIOD_END_LICENSE_Critical) as pointA, sum(OPEN_COUNT_AT_TIME_PERIOD_END_LICENSE_SEVERE) as pointB, sum(OPEN_COUNT_AT_TIME_PERIOD_END_LICENSE_MODERATE) as pointC from metric group by time_period_start";
 
 	
-	public static String MTTR = "select TIME_PERIOD_START as label, isnull(avg(MTTR_CRITICAL_THREAT)/(60*60*24*1000),0) as pointA, isnull(avg(MTTR_SEVERE_THREAT)/(60*60*24*1000),0) as pointB, isnull(avg(MTTR_MODERATE_THREAT)/(60*60*24*1000),0) as pointC from metric group by TIME_PERIOD_START";
+	public static String MTTR = "select TIME_PERIOD_START as label, avg(case when MTTR_CRITICAL_THREAT <>0 then MTTR_CRITICAL_THREAT else null end)/86400000 as pointA,  avg(case when MTTR_SEVERE_THREAT <> 0 then MTTR_SEVERE_THREAT else null end)/864000000 as pointB,  avg(case when MTTR_MODERATE_THREAT <> 0 then MTTR_MODERATE_THREAT else null end)/86400000  as pointC from metric  group by time_period_start";
 
 	public static String OrganisationsOpenViolations = "select  distinct organization_name as label, sum(OPEN_COUNT_AT_TIME_PERIOD_END_SECURITY_CRITICAL ) as pointA, sum(OPEN_COUNT_AT_TIME_PERIOD_END_SECURITY_SEVERE ) as pointB, sum(OPEN_COUNT_AT_TIME_PERIOD_END_SECURITY_MODERATE ) as pointC from metric";
 	
