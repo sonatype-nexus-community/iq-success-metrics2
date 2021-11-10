d7ebbfff75ade965e5e0647bd8064006eb8589d8
Sola Otudeko <sotudeko@sonatype.com>
2021-11-10 09:14:20 +0000

    84

--- releasefiles/reports2/create-data.sh
+++ releasefiles/reports2/create-data.sh
@@ -8,4 +8,3 @@ python3 create-application-evaluations-data.py ${iqurl} ${iquser} ${iqpwd}
 python3 create-policy-violations-data.py ${iqurl} ${iquser} ${iqpwd} 
 python3 create-components-quarantined.py ${iqurl} ${iquser} ${iqpwd}
 python3 create-waiver-data.py ${iqurl} ${iquser} ${iqpwd}
-python3 create-firewall-data.py ${iqurl} ${iquser} ${iqpwd}
